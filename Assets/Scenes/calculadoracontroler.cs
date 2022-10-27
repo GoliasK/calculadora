63a540c36baf74f58b75269a30561a722838ff5d
GoliasK <marcuspaulodiblopes@gmail.com>
2022-10-26 21:45:34 -0300

    mudar backgroud

--- Assets/Scenes/calculadoracontroler.cs
+++ Assets/Scenes/calculadoracontroler.cs
@@ -8,6 +8,18 @@ public class calculadoracontroler : MonoBehaviour
 {
     [SerializeField] private Text TextDisplay1; 
     [SerializeField] private Text TextDisplay2;
+    [SerializeField] private Color blackBackground = Color.black;
+    [SerializeField] private Color numberBlue = Color.blue;
+    [SerializeField] private Color buttomBlak = Color.black;
+    
+    
+    //Color Write
+    [SerializeField] private Color whiteBackground = Color.white;
+    
+    //UI Components.
+    [SerializeField] private Text[] font ;
+    [SerializeField] private Sprite spriteButtons;
+    [SerializeField] private Camera MainCamera;
   private string _expression;
 
 
@@ -38,6 +50,19 @@ public class calculadoracontroler : MonoBehaviour
       {
         TextDisplay1.text = "";
       }
+      public void ChoiceBackColor()
+    {
+        MainCamera.backgroundColor = blackBackground;
+        TextDisplay1.color = whiteBackground;
+        TextDisplay2.color = whiteBackground;
+    }
+
+    public void ChoicecWhiteColor()
+    {
+        MainCamera.backgroundColor = whiteBackground;
+        TextDisplay1.color = blackBackground;
+        TextDisplay2.color = blackBackground;
+    }  
     // Update is called once per frame
     void Update()
     {
